63380cfa60c4ad269800e18670eb11e240cfc4e2
Rishav <140265067+IamRishavDas@users.noreply.github.com>
2023-09-14 13:10:18 +0530

    Window for services commit

---
+++ SourceCode/Source/CreateAccount.java
@@ -0,0 +1,10 @@
+package CreateAccountWindow;
+
+import javax.swing.JPanel;
+
+public class CreateAccount {
+    public static JPanel createAccountPanel;
+    public static void main(String[] args) {
+        
+    }
+}
